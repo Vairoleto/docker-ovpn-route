ffeed6af917c703b9182df449398a7dd0f629b25
ggabas <ggabas@procomargentina.com>
2019-03-14 11:57:42 -0300

    fix silent iptables-persistent install

--- start.sh
+++ start.sh
@@ -91,7 +91,7 @@ chmod +x /usr/bin/openvpn
 docker_network
 # instalar iptables-persistent 
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 sudo apt-get -y install iptables-persistent
 }
 
